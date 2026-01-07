537624e0ca2f712c74012e2431efdf54223b95ac
Ankit-Raj27 <ankitrajk27@gmail.com>
2026-01-08 01:33:22 +0530

    feat: add initial user data configuration and name pronunciation audio file.

--- src/features/portfolio/data/user.ts
+++ src/features/portfolio/data/user.ts
@@ -41,7 +41,7 @@ about: `
   avatar: "https://i.ibb.co/cSTYyD2T/profile-portfolio.png",
   ogImage:
     "https://assets.chanhdai.com/images/screenshot-og-image-light.png?v=4",
-  namePronunciationUrl: "/audio/chanhdai.mp3",
+  namePronunciationUrl: "/audio/name.mp3",
   timeZone: "Asia/Kolkata",
   keywords: [
     "ncdai",
